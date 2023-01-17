a89562aa16ab66fee5d0c7231ab9700e1b4662e4
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-17 02:29:48 -0600

    Created Button component

---
+++ lettucequiz/src/components/Button.tsx
@@ -0,0 +1,12 @@
+function Button() {
+  return (
+    <button
+      type="button"
+      className="relative inline-flex justify-center items-center px-12 py-2 border border-transparent text-base font-semibold rounded-full shadow-sm text-gray-800 bg-cadetblue tracking-widest"
+    >
+      <p>NEXT QUESTION</p>
+    </button>
+  );
+}
+
+export default Button;
