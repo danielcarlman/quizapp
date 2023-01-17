6ac2203a705a3a71b73c90d12623888479149c29
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-17 01:43:16 -0600

    Added custom color cadetblue

--- lettucequiz/tailwind.config.cjs
+++ lettucequiz/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cadetblue: "#9db2be",
+      },
+    },
   },
   plugins: [],
 };
