1f6b0e6a8efe088f8a79902f5875742af4d15870
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-17 02:38:23 -0600

    Fixed button positioning

--- lettucequiz/src/components/Button.tsx
+++ lettucequiz/src/components/Button.tsx
@@ -2,7 +2,7 @@ function Button() {
   return (
     <button
       type="button"
-      className="relative inline-flex justify-center items-center px-12 py-2 border border-transparent text-base font-semibold rounded-full shadow-sm text-gray-800 bg-cadetblue tracking-widest -m-4"
+      className="relative bottom-6 inline-flex justify-center items-center px-12 py-2 border border-transparent text-base font-semibold rounded-full shadow-sm text-gray-800 bg-cadetblue tracking-widest"
     >
       <p>NEXT QUESTION</p>
     </button>
