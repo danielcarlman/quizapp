b856c3abbb642b7ba3d30568bf8c85a8cfbb532b
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-16 23:00:33 -0600

    Created header

--- lettucequiz/src/App.tsx
+++ lettucequiz/src/App.tsx
@@ -2,12 +2,15 @@ import background from "./assets/pattern.jpg";
 
 function App() {
   return (
-    <div
-      className="flex justify-center items-center h-screen bg-cover"
+    <header
+      className="flex flex-col justify-center items-center h-screen bg-cover p-4 space-y-4"
       style={{ backgroundImage: `url(${background})` }}
     >
-      <h1 className="text-4xl font-bold">Lettuce Feed Quiz</h1>
-    </div>
+      <h1 className="text-6xl font-bold tracking-widest">QUIZ</h1>
+      <h1 className="text-xl text-center">
+        Take our quiz to find out what restaurant you should try for dinner.
+      </h1>
+    </header>
   );
 }
 
