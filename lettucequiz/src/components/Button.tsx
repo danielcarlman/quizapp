110da7fb858c060e1ce53b7825a9bed95862738b
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-17 21:38:53 -0600

    Disabled button when no option is selected

--- lettucequiz/src/components/Button.tsx
+++ lettucequiz/src/components/Button.tsx
@@ -9,7 +9,9 @@ function Button({ onClick, disabled }: ButtonInterface) {
       onClick={onClick}
       type="button"
       disabled={disabled}
-      className="relative bottom-6 inline-flex justify-center items-center px-12 py-2 border border-transparent text-base font-semibold rounded-full shadow-sm text-gray-800 bg-cadetblue tracking-widest"
+      className={`relative bottom-6 inline-flex justify-center items-center px-12 py-2 border border-transparent text-base font-semibold rounded-full shadow-sm tracking-widest ${
+        disabled ? "bg-gray-200 text-gray-400" : "bg-cadetblue text-gray-800"
+      }`}
     >
       <p>NEXT QUESTION</p>
     </button>
