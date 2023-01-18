2d487668fc0afc81afa21cf339cf4517b5e1159f
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-17 20:30:22 -0600

    Added onChange handler for input

--- lettucequiz/src/components/Option.tsx
+++ lettucequiz/src/components/Option.tsx
@@ -1,12 +1,23 @@
 interface Option {
   value: string;
   points: any;
+  id: number;
 }
 
-function Option({ value, points }: Option) {
+function Option({ value, points, id }: Option) {
+  const handleOnChange = () => {
+    console.log("ID :", id);
+    console.log("Points :", points);
+  };
+
   return (
     <span className="flex space-x-2">
-      <input className="w-6 h-6" type="radio" name="option" />
+      <input
+        className="w-6 h-6"
+        type="radio"
+        name="option"
+        onChange={handleOnChange}
+      />
       <p>{value}</p>
       <p>{points}</p>
     </span>
