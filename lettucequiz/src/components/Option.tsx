abecf620c511a6c2b0d762980d9ac133526957d4
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-18 10:37:21 -0600

    Created replacement for radio button (WIP)

--- lettucequiz/src/components/Option.tsx
+++ lettucequiz/src/components/Option.tsx
@@ -1,18 +1,28 @@
 import useStore from "../store/store";
+import checked from "../assets/checked.svg";
+import unchecked from "../assets/unchecked.svg";
+import { useState } from "react";
 
 interface OptionProps {
   value: string;
-  points: any;
+  points: number;
 }
 
 function Option({ value, points }: OptionProps) {
+  // const [isChecked, setIsChecked] = useState(false);
   const setPoints = useStore((state) => state.setPoints);
   const handleOnChange = () => {
+    // setIsChecked(!isChecked);
     setPoints(points);
   };
 
   return (
     <label className="flex space-x-2">
+      {/* {isChecked ? (
+        <img src={checked} className="w-6 h-6" alt="checked" />
+      ) : (
+        <img src={unchecked} className="w-6 h-6" alt="unchecked" />
+      )} */}
       <input
         className="w-6 h-6"
         type="radio"
