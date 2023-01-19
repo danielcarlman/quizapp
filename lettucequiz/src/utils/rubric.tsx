abe9545a508031e1efd24aca820c5267698636cd
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-19 10:42:54 -0600

    Changed titles

--- lettucequiz/src/utils/rubric.tsx
+++ lettucequiz/src/utils/rubric.tsx
@@ -6,7 +6,7 @@ import tallboy from "../assets/tallboy.jpg";
 export default function rubric(totalPoints: number) {
   if (totalPoints > 21)
     return {
-      restaurant: "Seafood Restaurant",
+      restaurant: "RPM Seafood",
       image: rpmseafood,
       description:
         "This is the definition of high-class dining experience. One of our best restaurants features prawns, shrimps and pretty much all great creatures from under the sea!",
@@ -14,7 +14,7 @@ export default function rubric(totalPoints: number) {
 
   if (totalPoints > 16)
     return {
-      restaurant: "Pub Restaurant",
+      restaurant: "Hub 51",
       image: hub51,
       description:
         "This is the greatest Pub Restaurant ever in the world. Such an incredible experience dining here. Feel free to bring your family and have fun under the sun!",
@@ -22,14 +22,14 @@ export default function rubric(totalPoints: number) {
 
   if (totalPoints > 10)
     return {
-      restaurant: "Casual Restaurant",
+      restaurant: "Beatrix",
       image: beatrix,
       description:
         "This is the greatest Casual Beat Restaurant ever in the world. Such an incredible experience dining here. Feel free to bring your friends and guest and have great party!",
     };
 
   return {
-    restaurant: "Mexican Restaurant",
+    restaurant: "Tall Boy",
     image: tallboy,
     description:
       "This is the greatest Restaurant for Tacos ever in the world. Such an incredible authentic Mexican here. Feel free to bring your friends and guests. Gracias!",
