36c24d5b6bbaabf82fa0a207483e88c648722927
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-19 10:39:20 -0600

    Created ResultBox component

---
+++ lettucequiz/src/components/ResultBox.tsx
@@ -0,0 +1,22 @@
+import useStore from "../store/store";
+import rubric from "../utils/rubric";
+
+function Results() {
+  const totalPoints = useStore((state) => state.totalPoints);
+  const result = rubric(totalPoints);
+
+  return (
+    <div className="border-4 border-cadetblue solid bg-white p-4 space-y-4">
+      <h3 className="text-xl font-semibold tracking-widest">
+        {result.restaurant}
+      </h3>
+
+      <img src={result.image} alt="restaurant image" />
+      <div className="flex flex-col text-left text-sm pb-6 space-y-2 tracking-wide leading-6">
+        {result.description}
+      </div>
+    </div>
+  );
+}
+
+export default Results;
