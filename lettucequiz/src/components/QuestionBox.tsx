63e6ed61520f6001e5ea441077a436d569e8bae2
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-19 12:24:59 -0600

    Used Tailwind for casing instead of JS for better accessibility and fixed paddings

--- lettucequiz/src/components/QuestionBox.tsx
+++ lettucequiz/src/components/QuestionBox.tsx
@@ -8,11 +8,11 @@ interface QuestionBoxProps {
 function QuestionBox({ questions, currentQuestion }: QuestionBoxProps) {
   const entries = questions[currentQuestion].answers;
   return (
-    <div className="border-4 border-cadetblue solid bg-white p-4 divide-y-4 divide-cadetblue space-y-4">
-      <h3 className="text-xl font-semibold tracking-widest">
-        {questions[currentQuestion].question.toUpperCase()}
+    <div className="border-4 border-cadetblue solid bg-white p-4 divide-y-4 divide-cadetblue space-y-4  w-full">
+      <h3 className="text-xl font-semibold tracking-widest uppercase">
+        {questions[currentQuestion].question}
       </h3>
-      <div className="flex flex-col text-left p-8 space-y-2 tracking-wide">
+      <div className="flex flex-col text-left p-8 pl-4 space-y-2 tracking-wide">
         {Object.entries(entries).map(([key, value]) => (
           <Option key={key} value={key} points={value} />
         ))}
