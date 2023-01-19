42766d499676bbe4bf5a3917c3192c60e4b1ab79
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-01-19 12:22:07 -0600

    Included a fixed width for images and a max width for container

--- lettucequiz/src/components/ResultBox.tsx
+++ lettucequiz/src/components/ResultBox.tsx
@@ -6,12 +6,16 @@ function Results() {
   const result = rubric(totalPoints);
 
   return (
-    <div className="border-4 border-cadetblue solid bg-white p-4 space-y-4">
+    <div className="border-4 border-cadetblue solid bg-white p-4 space-y-4 max-w-lg">
       <h3 className="text-xl font-semibold tracking-widest">
         {result.restaurant}
       </h3>
 
-      <img src={result.image} alt="restaurant image" />
+      <img
+        src={result.image}
+        alt="restaurant image"
+        className="w-60 h-60 m-auto"
+      />
       <div className="flex flex-col text-left text-sm pb-6 space-y-2 tracking-wide leading-6">
         {result.description}
       </div>
